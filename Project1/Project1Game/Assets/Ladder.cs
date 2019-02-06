bcbb511a954890b1bb6125ed13e24cf243bdf580
Adam Booth <u1855554@unimail.hud.ac.uk>
2019-02-06 14:57:26 +0000

    added ladder to test and to level1

---
+++ Project1/Project1Game/Assets/Ladder.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ladder : MonoBehaviour
+{
+    public float speed = 5;
+
+    void Update()
+    {
+
+    }
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Player" && Input.GetKey(KeyCode.UpArrow))
+        {
+            other.GetComponent<Rigidbody2D>().velocity = new Vector2(0, speed);
+        }
+        else if (other.tag == "Player" && Input.GetKey(KeyCode.DownArrow))
+        {
+            other.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -speed);
+        }
+        else
+        {
+            other.GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
+        }
+    }
+}
