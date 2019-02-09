cc66b1ef7a6580decc3f7d89c75e0c82b3a5e4d6
Adam Booth <u1855554@unimail.hud.ac.uk>
2019-02-09 00:34:46 +0000

    started enemy bullets in testing

---
+++ Project1/Project1Game/Assets/scripts/cactusBullet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cactusBullet : MonoBehaviour
+{
+    public float speed = 15f;
+    Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = transform.right * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Project1/Project1Game/Assets/scripts/cactusShoot.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cactusShoot : MonoBehaviour
+{
+    public Transform enemyFirePoint;
+    public GameObject enemyBulletPrefab;
+
+    void Update()
+    {
+        wide();
+        StartCoroutine(waitTime());
+        narrow();
+        StartCoroutine(waitTime());
+    }
+
+    void wide()
+    {
+        Instantiate(enemyBulletPrefab, enemyFirePoint.position, Quaternion.Euler(0, 0, 135));
+        Instantiate(enemyBulletPrefab, enemyFirePoint.position, Quaternion.Euler(0, 0, 180));
+        Instantiate(enemyBulletPrefab, enemyFirePoint.position, Quaternion.Euler(0, 0, 225));
+    }
+
+    void narrow()
+    {
+        Instantiate(enemyBulletPrefab, enemyFirePoint.position, Quaternion.Euler(0, 0, 202));
+        Instantiate(enemyBulletPrefab, enemyFirePoint.position, Quaternion.Euler(0, 0, 152));
+    }
+
+    IEnumerator waitTime()
+    {
+        yield return new WaitForSeconds(2);
+    }
+}
